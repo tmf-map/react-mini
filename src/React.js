e3e2a72cbe2c2c93b7d77e6cab56308b894e9096
Kimi <mrgaonju@gmail.com>
2019-07-22 18:09:49 +0800

    vdom: add createElement and render function

--- src/React.js
+++ src/React.js
@@ -1,9 +1,15 @@
 function createElement(type, props, ...children) {
-  return {
+  const vdom = {
     type,
     props,
     children
   }
+  // 因为是浅冻结，故里外两层都得冻结
+  // 冻结后，不能增/删/改该属性
+  // 顺序无所谓
+  Object.freeze(vdom.props)
+  Object.freeze(vdom)
+  return vdom
 }
 
 export default {
